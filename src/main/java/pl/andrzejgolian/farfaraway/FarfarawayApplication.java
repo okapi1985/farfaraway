48ad4bd78443f93f92662fb9251ca78eefad4cad
okapi1985 <andrzej.golian@wp.pl>
2019-07-28 15:10:06 +0200

    initial commit

---
+++ src/main/java/pl/andrzejgolian/farfaraway/FarfarawayApplication.java
@@ -0,0 +1,13 @@
+package pl.andrzejgolian.farfaraway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarfarawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarfarawayApplication.class, args);
+	}
+
+}
